78251fae30f2201ea1405094dde6fa40e04510a0
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-03-23 19:00:29 +0000

    Missing commit.

---
+++ src/main/scala/input/sinks/OsmEntitySink.scala
@@ -0,0 +1,50 @@
+package input.sinks
+
+import java.text.DecimalFormat
+import java.util
+
+import org.joda.time.DateTime
+import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer
+import org.openstreetmap.osmosis.core.domain.v0_6._
+import org.openstreetmap.osmosis.core.task.v0_6.Sink
+
+class OsmEntitySink(predicate: Entity => Boolean, callback: Entity => Unit) extends Sink {
+
+  val step = 10000000
+  val nf = new DecimalFormat()
+
+  var i = 0L
+  var j = 0
+  var matched = 0L
+  var low = DateTime.now()
+
+  override def process(entityContainer: EntityContainer): Unit = {
+    val entity = entityContainer.getEntity
+    if (predicate(entity)) {
+      // println("Found: " + entity.getId + entity.getType + " " + entity.getTags.asScala.find(t =>t.getKey == "name").map(t => t.getValue))
+      matched = matched + 1
+      callback(entity)
+    }
+
+    if (j == step) {
+      val now = DateTime.now
+      val delta = now.getMillis - low.getMillis
+      val rate = step / (delta * 0.001)
+      println(nf.format(i) + ": " + delta + " @ " + nf.format(rate))
+      low = now
+      j = 0
+    }
+
+    i = i + 1
+    j = j +1
+  }
+
+  override def initialize(metaData: util.Map[String, AnyRef]) = {}
+
+  override def complete() = {}
+
+  override def close() = {
+    println("Processed: " + i + " entities and matched " + matched)
+  }
+
+}
---
+++ src/main/scala/resolving/OutlineBuilder.scala
@@ -0,0 +1,61 @@
+package resolving
+
+import org.openstreetmap.osmosis.core.domain.v0_6._
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
+
+class OutlineBuilder {
+
+  val relationResolver = new RelationWayResolver()
+
+  // Give a relation resolve it's outer to a seq of consecutively ordered points
+  def outlineNodesFor(r: Relation, allRelations: Map[Long, Relation], ways: Map[Long, Way], nodes: Map[Long, Node]): Seq[Node] = { // TODO handle missing Ways and nodes
+
+    // Attempt to join up the ways (which may be out of order and facing in different directions) into a list consecutive nodes
+    def joinWays(ways: Seq[Way]): Seq[Seq[Long]] = {
+      val nonEmptyWayGroups = ways.map { w =>
+        w.getWayNodes.asScala.map(wn => wn.getNodeId)
+      }.filter(wg => wg.nonEmpty)
+
+      if (nonEmptyWayGroups.nonEmpty) {
+        val available = mutable.Set() ++ nonEmptyWayGroups // TODO what of relationship has multiple rings; available will not be fully consumed
+
+        val first = available.head
+        var joined = Seq(first)
+        available.remove(first)
+
+        while (available.nonEmpty && available.exists(wg => wg.head == joined.last.last || wg.last == joined.last.last)) {
+          val next = available.find(wg => wg.head == joined.last.last || wg.last == joined.last.last).get
+          if (next.head == joined.last.last) {
+            joined = joined :+ next
+          } else {
+            joined = joined :+ next.reverse
+          }
+          available.remove(next)
+        }
+        joined
+
+      } else {
+        println("Relation has no non empty outers: " + r.getId)
+        Seq()
+      }
+    }
+
+    try {
+      val outerWays = relationResolver.resolveOuterWayIdsFor(r, allRelations).map { wid =>
+        ways.get(wid)
+      }.flatten // TODO handle missing ways
+
+      joinWays(outerWays).flatten.map { nid =>
+        nodes.get(nid)
+      }.flatten
+
+    } catch {
+      case e: Exception =>
+        println("Failed to compose relation: " + r.getId, e.getMessage)
+        throw (e)
+    }
+  }
+
+}
