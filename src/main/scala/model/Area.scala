f487c1211aed6d648085f45822b39f4124ebe851
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-03-28 13:16:33 +0100

    Pass OSM ID into graph.

--- src/main/scala/model/Area.scala
+++ src/main/scala/model/Area.scala
@@ -2,4 +2,4 @@ package model
 
 import com.esri.core.geometry.Polygon
 
-case class Area(name: String, polygon: Polygon, boundingBox: (Double, Double, Double, Double))
+case class Area(name: String, polygon: Polygon, boundingBox: (Double, Double, Double, Double), osmId: Option[String] = None)
