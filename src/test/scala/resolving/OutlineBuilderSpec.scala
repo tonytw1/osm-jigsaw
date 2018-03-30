ee2275a200ad02554e34fb0bdedeef8a5322a8e2
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-03-30 13:02:48 +0100

    Clean up.

--- src/test/scala/resolving/OutlineBuilderSpec.scala
+++ src/test/scala/resolving/OutlineBuilderSpec.scala
@@ -4,6 +4,8 @@ import input.TestValues
 import org.openstreetmap.osmosis.core.domain.v0_6.{Entity, Node, Relation, Way}
 import org.scalatest.FlatSpec
 
+import scala.collection.mutable
+
 class OutlineBuilderSpec extends FlatSpec with TestValues with LoadTestEntities {
 
   val outlineBuilder = new OutlineBuilder()
@@ -11,39 +13,30 @@ class OutlineBuilderSpec extends FlatSpec with TestValues with LoadTestEntities 
   "outline builder" should "assemble the outer ways of a relation into a consecutive list of points" in {
     val entities = loadTestData()
 
-    val relations = entities.flatMap { e =>
-      e match {
-        case r: Relation => Some(r)
-        case _ => None
-      }
-    }.toSet
+    val rs = mutable.Set[Relation]()
+    val ws = mutable.Set[Way]()
+    val ns = mutable.Set[Node]()
 
-    val ways: Map[Long, Way] = entities.flatMap { e =>
-      e match {
-        case w: Way => Some(w)
-        case _ => None
-      }
-    }.map { i =>
-      (i.getId, i)
-    }.toMap
+    entities.map {
+      case r: Relation => rs.+=(r)
+      case w: Way => ws.+=(w)
+      case n: Node => ns.+=(n)
+      case _ =>
+    }
 
-    val nodes = entities.flatMap { e =>
-      e match {
-        case n: Node => Some(n)
-        case _ => None
-      }
-    }.map { i =>
-      (i.getId, (i.getId, i.getLatitude, i.getLongitude))
-    }.toMap
+    val relations = rs.toSet
+    val ways = ws.map { i => (i.getId, i)}.toMap
+    val nodes = ns.map { i => (i.getId, (i.getId, i.getLatitude, i.getLongitude))}.toMap
 
-    val outline = outlineBuilder.outlineNodesFor(relations.head, relations.map(r => r.getId -> r).toMap, ways, nodes)
+    val richmond = relations.find(r => r.getId == LONDON_BOROUGH_OF_RICHMOND_UPON_THAMES_RELATION._1).head
+    val outline = outlineBuilder.outlineNodesFor(richmond, relations.map(r => r.getId -> r).toMap, ways, nodes)
 
     assert(outline.size == 1671)
   }
 
   def loadTestData(): Seq[Entity] = {
     def all(entity: Entity): Boolean = true // TODO how to inline a closure in Scala?
-    loadEntities("richmond.pbf", all)
+    loadEntities("gb-test-data.pbf", all)
   }
 
 }
