7d428534dc33b0be1fc9d2cb3720806c22674afe
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-04-13 23:36:04 +0100

    Are set operations slow because the GraphNode hashcode is slow?

--- src/main/scala/model/GraphNode.scala
+++ src/main/scala/model/GraphNode.scala
@@ -1,11 +1,15 @@
 package model
 
-case class GraphNode(area: Area, var children: Set[GraphNode] = Set()) {
+import java.util.UUID
+
+case class GraphNode(area: Area, var children: Set[GraphNode] = Set(), id: UUID = UUID.randomUUID()) {
 
   def insert(newArea: Area): GraphNode = {
     val newNode = GraphNode(newArea)
     children = children + newNode
     newNode
   }
+
+  override def hashCode(): Int = id.hashCode()
 
 }
