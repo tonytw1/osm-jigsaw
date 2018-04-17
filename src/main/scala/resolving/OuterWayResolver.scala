4b8f11764782fc45876754b46b529b03407f1064
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-04-17 07:40:34 +0100

    Logging.

--- src/main/scala/resolving/OuterWayResolver.scala
+++ src/main/scala/resolving/OuterWayResolver.scala
@@ -14,7 +14,7 @@ class OuterWayResolver extends Logging {
         case EntityType.Way => Seq(rm.getMemberId)
         case EntityType.Relation => // TODO want test case for this
           allRelations.get(rm.getMemberId).map { sr =>
-            logger.info("Relation " + r.getId + " has subrelation as an outer")
+            logger.info("Relation " + r.getId + " has subrelation " + rm.getMemberId + " as an outer")
             resolveOuterWayIdsFor(sr, allRelations)
           }.getOrElse {
             logger.warn("Could not resolve outer subrelation " + rm.getMemberId + " for relation " + r + "; ignoring this subrelation")
