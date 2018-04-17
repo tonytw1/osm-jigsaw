ceaa3d71a5a8d35eaafe9c07af30a57dcb0940d2
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-04-17 07:52:37 +0100

    Better circular reference detection.

--- src/main/scala/resolving/OuterWayResolver.scala
+++ src/main/scala/resolving/OuterWayResolver.scala
@@ -7,15 +7,25 @@ import scala.collection.JavaConverters._
 
 class OuterWayResolver extends Logging {
 
-  def resolveOuterWayIdsFor(r: Relation, allRelations: Map[Long, Relation]): Seq[Long] = {
-    val outers = r.getMembers.asScala.filterNot(rm => rm.getMemberType == EntityType.Relation && rm.getMemberId == r.getId).filter(rm => rm.getMemberRole == "outer")
+  def resolveOuterWayIdsFor(r: Relation, allRelations: Map[Long, Relation], relationSeenSoFar: Set[Long] = Set()): Seq[Long] = {
+
+    val usedRelations = relationSeenSoFar + r.getId
+
+    val outers = r.getMembers.asScala.filterNot{ rm =>
+      val isCircularReference = rm.getMemberType == EntityType.Relation && usedRelations.contains(rm.getMemberId)
+      if (isCircularReference) {
+        logger.warn("Relation " + r.getId + " has suspected circular reference to relation: " + rm.getMemberId)
+      }
+      isCircularReference
+
+    }.filter(rm => rm.getMemberRole == "outer")
     outers.flatMap { rm =>
       rm.getMemberType match {
         case EntityType.Way => Seq(rm.getMemberId)
         case EntityType.Relation => // TODO want test case for this
           allRelations.get(rm.getMemberId).map { sr =>
             logger.info("Relation " + r.getId + " has subrelation " + rm.getMemberId + " as an outer")
-            resolveOuterWayIdsFor(sr, allRelations)
+            resolveOuterWayIdsFor(sr, allRelations, usedRelations)
           }.getOrElse {
             logger.warn("Could not resolve outer subrelation " + rm.getMemberId + " for relation " + r + "; ignoring this subrelation")
             Seq()
