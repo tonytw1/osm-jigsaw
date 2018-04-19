f9d3ea7424231537681bcfa80ae2ee9cb48c0243
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-04-19 14:49:42 +0100

    Disable bounding box check.

--- src/main/scala/areas/AreaComparision.scala
+++ src/main/scala/areas/AreaComparision.scala
@@ -8,14 +8,15 @@ trait AreaComparison {
   val sr = SpatialReference.create(1)
 
   def areaContains(a: Area, b: Area): Boolean = {
-    if (a.boundingBox._3 < b.boundingBox._1 || a.boundingBox._1 > b.boundingBox._3 || a.boundingBox._2 < b.boundingBox._4 || a.boundingBox._4 > b.boundingBox._2) {
-      false
-    } else {
+    //if (a.boundingBox._3 < b.boundingBox._1 || a.boundingBox._1 > b.boundingBox._3 || a.boundingBox._2 < b.boundingBox._4 || a.boundingBox._4 > b.boundingBox._2) {
+    //  false
+    //} else {
       OperatorContains.local().execute(a.polygon, b.polygon, sr, null) && !OperatorContains.local().execute(b.polygon, a.polygon, sr, null)
       // logger.info("!!!! " + a.name + " v " + b.name + ": " + r)
-    }
+    //}
   }
 
+  /*
   def areasOverlap(a: Area, b: Area) = {
     if (a.boundingBox._3 < b.boundingBox._1 || a.boundingBox._1 > b.boundingBox._3 || a.boundingBox._2 < b.boundingBox._4 || a.boundingBox._4 > b.boundingBox._2) {
       false
@@ -23,6 +24,7 @@ trait AreaComparison {
       OperatorOverlaps.local().execute(a.polygon, b.polygon, sr, null)
     }
   }
+  */
 
   def areaOf(area: Area): Double = {
     Math.abs(area.polygon.calculateArea2D())
