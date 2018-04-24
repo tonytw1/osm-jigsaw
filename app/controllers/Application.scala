13966b911952cb8e6bf550a9304aa66a47667350
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-04-24 19:55:36 +0100

    Can parse pbf graph file into memory

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,19 +1,34 @@
-
 package controllers
+
+import java.io.{BufferedInputStream, File, FileInputStream}
 
 import outputarea.{OutputArea, OutputPoint}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 
+import scala.collection.mutable
 import scala.concurrent.Future
 
 class Application extends Controller {
 
+  val areas = mutable.ListBuffer[OutputArea]()
+
+  {
+    val file = new File("great-britain-latest.graph.pbf")
+    val is = new BufferedInputStream(new FileInputStream(file))
+
+    var ok = true
+    while (ok) {
+      val area = OutputArea.parseDelimitedFrom(is)
+      area.map(a => areas += a)
+      ok = area.nonEmpty
+    }
+  }
+
   def ping = Action.async { request =>
-    val area = new OutputArea(name = Some("protobuf class work"))
     implicit val opw = Json.writes[OutputPoint]
     implicit val oaw = Json.writes[OutputArea]
-    Future.successful(Ok(Json.toJson(area)))
+    Future.successful(Ok(Json.toJson(areas.headOption)))
   }
 
 }
