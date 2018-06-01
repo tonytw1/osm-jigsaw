22d69c6c926733414e4e7196379fe8a9e831983d
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-06-01 17:23:22 +0100

    End to end run

---
+++ go.bash
@@ -0,0 +1,8 @@
+input="great-britain-latest"
+jarfile="osm-jigsaw-assembly-0.1.0-SNAPSHOT.jar"
+java -jar $jarfile -s split $input.osm.pbf
+java -Xmx28G -jar $jarfile -s extract $input.osm.pbf $input.rels.pbf
+java -Xmx16G -jar $jarfile -s areas $input.rels.pbf $input.areas.ser
+java -Xmx28G -jar $jarfile -s graph $input.areas.ser $input.graph.ser
+java -Xmx28G -jar $jarfile -s export $input.graph.ser $input.graph.pbf
+
