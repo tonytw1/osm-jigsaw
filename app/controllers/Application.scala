0754260b082a24850f40827ae6a799c07ddd13c7
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-06-15 14:13:40 +0100

    Spike out reverse geocode of lat long to a list of possible paths through the graph down to areas which enclose that point.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,14 +2,18 @@ package controllers
 
 import javax.inject.Inject
 
+import com.esri.core.geometry.{OperatorContains, Point, SpatialReference}
 import graph.{Area, GraphService}
-import play.api.Configuration
+import play.api.libs.json.Json
+import play.api.{Configuration, Logger}
 import play.api.mvc.{Action, Controller}
 
 import scala.collection.mutable
 import scala.concurrent.Future
 
 class Application @Inject()(configuration: Configuration, graphService: GraphService) extends Controller {
+
+  val sr = SpatialReference.create(1)
 
   def index(qo: Option[String]) = Action.async { request =>
 
@@ -29,7 +33,50 @@ class Application @Inject()(configuration: Configuration, graphService: GraphSer
       areas.+=(show)
     }
 
+    Logger.info("Areas: " + areas.map(a => a.name).mkString(" / "))
+    val lastArea = areas.last
+    Logger.info("Last area: " + lastArea.name)
+
     Future.successful(Ok(views.html.index(areas, show)))
+  }
+
+  def reverse(lat: Double, lon: Double) = Action.async { request =>
+
+    def areasContaining(pt: Point, area: Area, stack: Seq[Area]): Seq[Seq[Area]] = {
+      Logger.info("Checking area: " + renderAreaStack(stack) + " / " + area.name.getOrElse(""))
+
+      val matchingChildren = area.children.toSeq.filter { c =>
+        c.polygon.map { p =>
+          OperatorContains.local().execute(p, pt, sr, null)
+        }.getOrElse {
+          Logger.warn("Area has no polygon: " + c.name)
+          false
+        }
+      }
+
+      if (matchingChildren.nonEmpty) {
+        matchingChildren.flatMap { m =>
+          areasContaining(pt, m, stack :+ area)
+        }
+      } else {
+        Seq(stack :+ area)
+      }
+    }
+
+    val pt = new Point(lat, lon)
+    val output = areasContaining(pt, graphService.head.children.head, Seq()).map { a =>
+      renderAreaStack(a)
+    }
+
+    Future.successful(Ok(Json.toJson(output)))
+  }
+
+  private def renderAreaStack(stack: Seq[Area]) = {
+    stack.map(a => a.name.getOrElse("?")).mkString(" / ")
+  }
+
+  private def areaContainsPoint(area: Area, pt: Point) = {
+    OperatorContains.local().execute(area.polygon.get, pt, sr, null)  // TODO naked get
   }
 
 }
