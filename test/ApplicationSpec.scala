f32c78dc3dda3b25fe330ad15e94679429ea4cb3
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-06-18 09:22:06 +0100

    Revert "Not a useful CI test as presented."
    This reverts commit 4f87a5abc8148517c8e114c123b3ef587dd61fe9.

---
+++ test/ApplicationSpec.scala
@@ -0,0 +1,15 @@
+import java.io.{BufferedInputStream, File}
+import java.net.URL
+
+import graph.GraphReader
+import org.specs2.mutable._
+
+class ApplicationSpec extends Specification {
+
+  "build graph from pbf" in {
+    val file = new URL(" http://10.0.45.22:32680/osm/great-britain-latest.graph.pbf")
+    val head = new GraphReader().loadGraph(new BufferedInputStream(file.openStream()))
+    1 must equalTo(1)
+  }
+
+}
