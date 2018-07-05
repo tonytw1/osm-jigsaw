156d921950536e16c9a1ff12430c64b3e1705383
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-07-05 10:07:07 +0100

    Indexing the tag key and values get us down downt from 11.9Gb to 8.2Gb of heap.

--- app/tags/TagService.scala
+++ app/tags/TagService.scala
@@ -4,26 +4,29 @@ import java.io.BufferedInputStream
 import java.net.URL
 import javax.inject.Inject
 
-import graph.Area
 import outputtagging.OutputTagging
 import play.api.{Configuration, Logger}
 import progress.ProgressCounter
 
-import scala.collection.mutable
+import scala.collection.{immutable, mutable}
 
 class TagService @Inject()(configuration: Configuration) {
 
   val tagsFile = new URL(configuration.getString("tags.url").get)
 
-  val tagsMap= loadTags(tagsFile)
+  val tagsMap = loadTags(tagsFile)
 
   def tagsFor(osmId: String): Option[Seq[(String, String)]] = {
-    tagsMap.get(osmId)
+    val keysIndex = tagsMap._2    // TODO push up
+    val valuesIndex = tagsMap._3    // TODO push up
+
+    tagsMap._1.get(osmId).map { i: Seq[(Int, Int)] =>
+      i.map( j => (keysIndex(j._1), valuesIndex(j._2)))
+    }
   }
 
-  private def loadTags(tagsFile: URL): Map[String, Seq[(String, String)]] = {
+  private def loadTags(tagsFile: URL): (Map[String, Seq[(Int, Int)]], immutable.IndexedSeq[String], immutable.IndexedSeq[String]) = {
     try {
-      val output = mutable.Map[String, Seq[(String, String)]]()
       val uniqueKeys = mutable.Set[String]()
       val uniqueValues = mutable.Set[String]()
 
@@ -31,8 +34,6 @@ class TagService @Inject()(configuration: Configuration) {
       var totalValues = 0
 
       val input = new BufferedInputStream(tagsFile.openStream())
-
-      val areasMap = mutable.Map[Long, Area]()
       val counter = new ProgressCounter(step = 10000, label = Some("Reading tags"))
       var ok = true
       while (ok) {
@@ -43,22 +44,47 @@ class TagService @Inject()(configuration: Configuration) {
             val keys = ot.keys
             val values = ot.values
 
-            keys.map( k => uniqueKeys.add(k))
-            values.map( k => uniqueValues.add(k))
+         keys.map( k => uniqueKeys.add(k))
+         values.map( k => uniqueValues.add(k))
             totalKeys = totalKeys + keys.size
             totalValues = totalValues + values.size
-
-            output.put(osmId, keys.zip(values))
-
           }
           ok = outputTagging.nonEmpty
         }
       }
+      input.close()
 
-      Logger.info("Read tags: " + output.size)
       Logger.info("Found " + totalKeys + " keys of which " + uniqueKeys.size + " were unique")
       Logger.info("Found " + totalValues + " values of which " + uniqueValues.size + " were unique")
-      output.toMap
+
+      Logger.info("Building key and value index maps")
+      val keysSeq = uniqueKeys.toIndexedSeq
+      val valuesSeq = uniqueValues.toIndexedSeq
+
+      val keysIndex: Map[String, Int] = keysSeq.zipWithIndex.toMap
+      val valuesIndex: Map[String, Int] = valuesSeq.zipWithIndex.toMap
+
+      Logger.info("Rereading tags after indexing")
+      val tagsMap = mutable.Map[String, Seq[(Int, Int)]]()
+
+      val input2 = new BufferedInputStream(tagsFile.openStream())
+      val counter2 = new ProgressCounter(step = 10000, label = Some("Reading tags"))
+      ok = true
+      while (ok) {
+        counter2.withProgress {
+          val outputTagging = OutputTagging.parseDelimitedFrom(input2)
+          outputTagging.map { ot =>
+            val osmId = ot.osmId.get
+            val keys: Seq[Int] = ot.keys.map(k => keysIndex.get(k).get)
+            val values: Seq[Int] = ot.values.map(v => valuesIndex.get(v).get)
+            tagsMap.put(osmId, keys.zip(values))
+          }
+          ok = outputTagging.nonEmpty
+        }
+      }
+      input2.close()
+
+      (tagsMap.toMap, keysSeq, valuesSeq)
 
     } catch {
       case e: Exception =>
