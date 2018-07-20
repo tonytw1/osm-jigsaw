73ce696e701da19ea5d7c1542642ea163e62ce83
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-07-20 21:11:37 +0100

    Upto date run script.

--- osm-jigsaw-parser/go.bash
+++ osm-jigsaw-parser/go.bash
@@ -1,8 +1,7 @@
-input="ireland-and-northern-ireland-180717"
+input="great-britain-180719"
 jarfile="osm-jigsaw-assembly-0.1.0-SNAPSHOT.jar"
-#java -jar $jarfile -s split $input.osm.pbf
+java -jar $jarfile -s split $input.osm.pbf
 java -Xmx16G -jar $jarfile -s extract $input.osm.pbf $input.rels.pbf
-#java -Xmx16G -jar $jarfile -s areas $input.rels.pbf $input.areas.ser
-#java -jar $jarfile -s tags $input.rels.pbf $input.tags.pbf
-#java -Xmx16G -jar $jarfile -s graph $input.areas.pbf $input.graph.pbf
-#java -Xmx28G -jar $jarfile -s export $input.graph.ser $input.graph.pbf
+java -Xmx16G -jar $jarfile -s areas $input.rels.pbf $input.areas.pbf
+java -jar $jarfile -s tags $input.rels.pbf $input.areas.pbf $input.tags.pbf
+time java -Xmx16G -jar $jarfile -s graph $input.areas.pbf $input.graph.pbf
