701f99fe50f6cab5fe612d019fcf7f5170c1b7f5
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-07-26 10:03:39 +0100

    Wishlist test; overlapping areas should both be able to contribute to a placename.

--- osm-jigsaw-api/test/naming/NaiveNamingServiceSpec.scala
+++ osm-jigsaw-api/test/naming/NaiveNamingServiceSpec.scala
@@ -1,17 +1,19 @@
 package naming
 
 import model.OsmId
+import org.mockito.Matchers.any
 import org.mockito.Mockito
 import org.specs2.mutable._
 import tags.TagService
 
-class NaiveNamingServiceSpec extends Specification  {
+class NaiveNamingServiceSpec extends Specification {
 
-  /*
+  private val R = "R".charAt(0)
+
   "place name is a concatenation of the the enclosing area names" in {
-    val australia = OsmId(80500L, "R".charAt(0))
-    val westernAustralia = OsmId(2316598, "R".charAt(0))
-    val ngaanyatjarra = OsmId(8165171, "R".charAt(0))
+    val australia = OsmId(80500L, R)
+    val westernAustralia = OsmId(2316598, R)
+    val ngaanyatjarra = OsmId(8165171, R)
 
     val paths = Seq(Seq(Seq(australia), Seq(westernAustralia), Seq(ngaanyatjarra)))
     val tagServiceMock = org.mockito.Mockito.mock(classOf[TagService])
@@ -19,6 +21,8 @@ class NaiveNamingServiceSpec extends Specification  {
     Mockito.when(tagServiceMock.nameForOsmId(australia)).thenReturn(Some("Australia"))
     Mockito.when(tagServiceMock.nameForOsmId(westernAustralia)).thenReturn(Some("Western Australia"))
     Mockito.when(tagServiceMock.nameForOsmId(ngaanyatjarra)).thenReturn(Some("Ngaanyatjarra Indigenous Protected Area"))
+    Mockito.when(tagServiceMock.tagsFor(any[OsmId])).thenReturn(None)
+
     val namingService = new NaiveNamingService(tagServiceMock)
 
     val name = namingService.nameFor(paths)
@@ -27,10 +31,10 @@ class NaiveNamingServiceSpec extends Specification  {
   }
 
   "consecutive duplicated place names are not adding value and can be removed" in {
-    val isleOfManAdminBoundary = OsmId(62269, "R".charAt(0))
-    val isleOfManIsland = OsmId(6041206, "R".charAt(0))
-    val middle = OsmId(1061146, "R".charAt(0))
-    val douglas = OsmId(1061138, "R".charAt(0))
+    val isleOfManAdminBoundary = OsmId(62269, R)
+    val isleOfManIsland = OsmId(6041206, R)
+    val middle = OsmId(1061146, R)
+    val douglas = OsmId(1061138, R)
 
     val paths = Seq(Seq(Seq(isleOfManAdminBoundary), Seq(isleOfManIsland), Seq(middle), Seq(douglas)))
     val tagServiceMock = org.mockito.Mockito.mock(classOf[TagService])
@@ -39,12 +43,37 @@ class NaiveNamingServiceSpec extends Specification  {
     Mockito.when(tagServiceMock.nameForOsmId(isleOfManIsland)).thenReturn(Some("Isle of Man"))
     Mockito.when(tagServiceMock.nameForOsmId(middle)).thenReturn(Some("Middle"))
     Mockito.when(tagServiceMock.nameForOsmId(douglas)).thenReturn(Some("Douglas"))
+    Mockito.when(tagServiceMock.tagsFor(any[OsmId])).thenReturn(None)
+
     val namingService = new NaiveNamingService(tagServiceMock)
 
     val name = namingService.nameFor(paths)
 
     name must equalTo("Douglas, Middle, Isle of Man")
   }
-  */
+
+  "need to consider overlappingareas" in {
+    val unitedStates = OsmId(148838, R)
+    val california = OsmId(165475, R)
+    val mariposaCounty = OsmId(396465, R)
+    val yosemite = OsmId(1643367, R)
+
+    val mariposaPath = Seq(Seq(unitedStates), Seq(california), Seq(mariposaCounty))
+    val yosemitePath = Seq(Seq(unitedStates), Seq(california), Seq(yosemite))
+
+    val paths = Seq(mariposaPath, yosemitePath)
+    val tagServiceMock = org.mockito.Mockito.mock(classOf[TagService])
+
+    Mockito.when(tagServiceMock.nameForOsmId(unitedStates)).thenReturn(Some("United States of America"))
+    Mockito.when(tagServiceMock.nameForOsmId(california)).thenReturn(Some("California"))
+    Mockito.when(tagServiceMock.nameForOsmId(mariposaCounty)).thenReturn(Some("Mariposa County"))
+    Mockito.when(tagServiceMock.nameForOsmId(yosemite)).thenReturn(Some("Yosemite National Park"))
+
+    val namingService = new NaiveNamingService(tagServiceMock)
+
+    val name = namingService.nameFor(paths)
+
+    name must contain("Yosemite National park")
+  }
 
 }
