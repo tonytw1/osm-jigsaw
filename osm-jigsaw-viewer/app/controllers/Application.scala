930a972a9f164ffe2f528c4176f7c534fbc5154a
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-07-30 11:22:48 +0100

    Revert "Debug."
    This reverts commit 6bfbc949314c7824d1151240ef817dc6738d47d8.

--- osm-jigsaw-viewer/app/controllers/Application.scala
+++ osm-jigsaw-viewer/app/controllers/Application.scala
@@ -21,10 +21,7 @@ class Application @Inject()(configuration: Configuration, ws: WSClient) extends 
   def index(qo: Option[String]) = Action.async { request =>
     val q = qo.getOrElse("")
 
-    val showEndpoint = apiUrl + "/show"
-    Logger.info("Calling API: " + showEndpoint)
-
-    ws.url(showEndpoint.addParam("q", q)).get.flatMap { r =>
+    ws.url((apiUrl + "/show").addParam("q", q)).get.flatMap { r =>
       implicit val er = Json.reads[Entity]
       implicit val gnr = Json.reads[GraphNode]
       val graphNodes = Json.parse(r.body).as[Seq[GraphNode]]
