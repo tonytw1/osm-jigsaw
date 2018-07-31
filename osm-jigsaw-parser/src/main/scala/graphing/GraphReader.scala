aef8a8d486b8156487ee54891ccf4a0cd6dac353
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-07-31 11:58:06 +0100

    Clean up; unused.

--- osm-jigsaw-parser/src/main/scala/graphing/GraphReader.scala
+++ osm-jigsaw-parser/src/main/scala/graphing/GraphReader.scala
@@ -2,14 +2,11 @@ package graphing
 
 import java.io.OutputStream
 
-import com.esri.core.geometry.SpatialReference
 import model.GraphNode
 import outputgraphnode.OutputGraphNode
 import progress.ProgressCounter
 
 class GraphReader {
-
-  val sr = SpatialReference.create(1)
 
   def export(node: GraphNode, output: OutputStream, parent: Option[Long], count: ProgressCounter): Unit = {
     count.withProgress {
