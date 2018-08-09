3767e12112fe71a5f0d599d8ed23fde09d299a04
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-08-09 15:32:48 +0100

    Route static assets and fixage.

--- osm-jigsaw-viewer/public/scripts/app.js
+++ osm-jigsaw-viewer/public/scripts/app.js
@@ -7,15 +7,6 @@ function resolvePoint(lat, lon) {
     });
 }
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: '@mapBoxApiKey'
-}).addTo(mymap);
-
 mymap.on('click', function(e) {
     var latlng = e.latlng.wrap();
     resolvePoint(latlng.lat, latlng.lng);
