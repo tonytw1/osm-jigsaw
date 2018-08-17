3b9020584d18caf42cfe8b86da3b454fe5ad451d
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-08-17 18:21:41 +0100

    Location error UI.

--- osm-jigsaw-viewer/public/scripts/app.js
+++ osm-jigsaw-viewer/public/scripts/app.js
@@ -21,14 +21,15 @@ var geoOptions = {
 var locate = $('#locate');
 locate.on('click', function(e) {
   e.preventDefault();
+  $('#locate-error').html("");
 
   function showPosition(position) {
       resolvePoint(position.coords.latitude,  position.coords.longitude);
       // TODO zoom to enclose the smallest component of the resolved name; API will need to make this available
   }
 
-  function showError() {
-    // TODO Show field error message
+  function showError(positionError) {
+    $('#locate-error').html("<p>Failed to get position: " + positionError.message + "</p>");
   }
 
   navigator.geolocation.getCurrentPosition(showPosition, showError, geoOptions);
