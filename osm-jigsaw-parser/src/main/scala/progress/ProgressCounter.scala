b920bef8d0f25c43237cdc441fc439665cc7f46d
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-08-20 10:55:45 +0100

    Just use millis.

--- osm-jigsaw-parser/src/main/scala/progress/ProgressCounter.scala
+++ osm-jigsaw-parser/src/main/scala/progress/ProgressCounter.scala
@@ -1,26 +1,23 @@
 package progress
 
-import java.text.DecimalFormat
-
 import org.apache.logging.log4j.scala.Logging
-import org.joda.time.DateTime
 
 class ProgressCounter(step: Int, total: Option[Long] = None, label: Option[String] = None) extends Logging with CommaFormattedNumbers {
 
   var i = 0L
   var j = 0
-  var low = DateTime.now()
+  var start = System.currentTimeMillis()
 
   def withProgress[R](f: => R): R = {
     i = i + 1
     j = j + 1
     if (j == step) {
-      val now = DateTime.now
-      val delta = now.getMillis - low.getMillis
+      val now = System.currentTimeMillis()
+      val delta = now - start
       val rate = step / (delta * 0.001)
 
       logger.info(label.map(l => l + " ").getOrElse("") + commaFormatted(i) + total.map(t => " / " + commaFormatted(t)).getOrElse("") + " in " + delta + "ms @ " + nf.format(rate) + " per second")
-      low = now
+      start = now
       j = 0
     }
     f
