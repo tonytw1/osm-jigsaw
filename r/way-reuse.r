521aa30533eb05ac2e90c75b925ebb595a187579
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-08-21 09:55:47 +0100

    Graph can be built from command line with Rscript

--- r/way-reuse.r
+++ r/way-reuse.r
@@ -2,16 +2,12 @@ library(dplyr)
 
 way_usages <- read.csv('area-ways.csv')
 
-reuse <- way_usages  %>% group_by(WAY) %>% summarise(count = n())
+reuse <- way_usages  %>% group_by_at(2) %>% summarise(count = n())
 
 h <- hist(reuse$count, ,breaks=seq(1,50,1))
-h$counts
-
-h <- hist(reuse$count, ,breaks=c(1,2,50))
 h$counts
 
 library(ggplot2)
 qplot(reuse$count, geom='histogram', breaks=seq(1,50,1), xlim=c(0,10), main = "Way reuse", xlab="Usages", ylab="Count")
 ggplot(data = reuse, aes(reuse$count)) + geom_histogram(breaks=seq(1,50,1))
-ggsave("out.png", width=12, height=8, units="cm", dpi=150)
-
+ggsave("way-reuse.png", width=12, height=8, units="cm", dpi=150)
