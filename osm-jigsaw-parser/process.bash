8a8497cc8377aa7c6fc82b5ca78639fbb47b5689
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-27 16:42:58 +0100

    Spike

--- osm-jigsaw-parser/process.bash
+++ osm-jigsaw-parser/process.bash
@@ -1,4 +1,13 @@
 FILE=$1
 echo $FILE
-ls -l /data
+cd /data
+
+SCRIPT=/opt/docker/bin/osm-jigsaw-parser
+
+time $SCRIPT -s split $input.osm.pbf
+time $SCRIPT -s extract $input.osm.pbf $input.rels.pbf
+time $SCRIPT -s areaways $input.rels.pbf $input.areaways.pbf
+time $SCRIPT -s areas $input.rels.pbf $input.areaways.pbf $input.areas.pbf
+time $SCRIPT -s graph $input.areas.pbf $input.graph.pbf
+time $SCRIPT -s tags $input.rels.pbf $input.areas.pbf $input.osm.pbf.named-nodes $input.tags.pbf
 
