6bc6f6e88a30c2626733fd5a53f5ceef50c17c51
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-27 17:26:10 +0100

    Spike

--- osm-jigsaw-parser/process.bash
+++ osm-jigsaw-parser/process.bash
@@ -7,5 +7,5 @@ time $SCRIPT -s split $input.osm.pbf
 time $SCRIPT -s extract $input.osm.pbf $input.rels.pbf
 time $SCRIPT -s areaways $input.rels.pbf $input.areaways.pbf
 time $SCRIPT -s areas $input.rels.pbf $input.areaways.pbf $input.areas.pbf
-time $SCRIPT -s graph $input.areas.pbf $input.graph.pbf
+time $SCRIPT -XX:+UseConcMarkSweepGC -s graph $input.areas.pbf $input.graph.pbf
 time $SCRIPT -s tags $input.rels.pbf $input.areas.pbf $input.osm.pbf.named-nodes $input.tags.pbf
