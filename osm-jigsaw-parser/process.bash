8a36e749527b9e423cb6e42ac4c6a5f7e45258d0
Tony McCrae <tony@eelpieconsulting.co.uk>
2018-09-27 17:27:29 +0100

    Spike

--- osm-jigsaw-parser/process.bash
+++ osm-jigsaw-parser/process.bash
@@ -3,9 +3,9 @@ cd /data
 
 SCRIPT=/opt/docker/bin/osm-jigsaw-parser
 
-time $SCRIPT -s split $input.osm.pbf
-time $SCRIPT -s extract $input.osm.pbf $input.rels.pbf
-time $SCRIPT -s areaways $input.rels.pbf $input.areaways.pbf
-time $SCRIPT -s areas $input.rels.pbf $input.areaways.pbf $input.areas.pbf
-time $SCRIPT -XX:+UseConcMarkSweepGC -s graph $input.areas.pbf $input.graph.pbf
-time $SCRIPT -s tags $input.rels.pbf $input.areas.pbf $input.osm.pbf.named-nodes $input.tags.pbf
+#time $SCRIPT -s split $input.osm.pbf
+#time $SCRIPT -s extract $input.osm.pbf $input.rels.pbf
+#time $SCRIPT -s areaways $input.rels.pbf $input.areaways.pbf
+#time $SCRIPT -s areas $input.rels.pbf $input.areaways.pbf $input.areas.pbf
+time $SCRIPT -XX:+PrintCommandLineFlags -XX:+UseConcMarkSweepGC -XX:MaxRAMPercentage=100 -s graph $input.areas.pbf $input.graph.pbf
+#time $SCRIPT -s tags $input.rels.pbf $input.areas.pbf $input.osm.pbf.named-nodes $input.tags.pbf
