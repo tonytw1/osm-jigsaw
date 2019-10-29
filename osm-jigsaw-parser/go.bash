86aadcf9289af5a653f5e2a72197e480915935d6
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-10-29 17:44:25 +0000

    Stop on errors.

--- osm-jigsaw-parser/go.bash
+++ osm-jigsaw-parser/go.bash
@@ -1,10 +1,12 @@
-input="great-britain-190827"
+# exit when any command fails
+set -e
+
+input="great-britain-191028"
 jarfile="osm-jigsaw-parser-assembly-1.0.jar"
 
-#time java -jar $jarfile -s boundaries $input
+java -jar $jarfile -s boundaries $input
 java -Xmx8G -jar $jarfile -s extract $input
-#java -Xmx8G -jar $jarfile -s areaways $input
-#java -Xmx8G -jar $jarfile -s areas $input
-#time java -Xmx8G -jar $jarfile -s graph $input.areas.pbf $input.graph.pbf
-
+java -Xmx8G -jar $jarfile -s areaways $input
+java -Xmx8G -jar $jarfile -s areas $input
+java -Xmx8G -jar $jarfile -s graph $input.areas.pbf $input.graph.pbf
 java -jar $jarfile -s tags $input $input.tags.pbf
