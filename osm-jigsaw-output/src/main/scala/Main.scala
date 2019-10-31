509003878459b4d5d3ff7eff97b36e68d92f2a21
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-10-31 12:58:52 +0000

    Refactor; extract render group block.

--- osm-jigsaw-output/src/main/scala/Main.scala
+++ osm-jigsaw-output/src/main/scala/Main.scala
@@ -34,25 +34,27 @@ object Main extends Logging {
     val tagsService = new TagService(tagsFile.toURI.toURL)
     val namingService = new NaiveNamingService(tagsService)
 
+    def renderGroup(group: ListBuffer[Seq[GraphNode]]): String = {
+      val x: mutable.Seq[Seq[(Seq[OsmId], Double)]] = group.map { i =>
+        i.map { j =>
+          (j.area.osmIds, j.area.area)
+        }.reverse
+      }
+
+      val osmIds = group.head.head.area.osmIds.map(_.render)
+      namingService.nameFor(x) + " (" + osmIds.mkString(",") + ")"
+    }
+
     var current: Long = 0
     var group: ListBuffer[Seq[GraphNode]] = ListBuffer.empty
     sorted.foreach{ i =>
       val justFinishedBlock = current > 0 && current != i.head.area.id
       if (justFinishedBlock) {
-        val x: mutable.Seq[Seq[(Seq[OsmId], Double)]] = group.map { i =>
-          i.map { j =>
-            (j.area.osmIds, j.area.area)
-          }.reverse
-        }
-
-        val osmIds = group.head.head.area.osmIds.map(_.render)
-        println(namingService.nameFor(x) + " (" + osmIds.mkString(",") + ")")
+        println(renderGroup(group))
         group = ListBuffer.empty
-
       }
       current = i.head.area.id
       group = group += i
-
     }
 
     logger.info("Done: " + traces.size)
@@ -61,10 +63,6 @@ object Main extends Logging {
   def descend(node: GraphNode, parents: Seq[GraphNode], traces: ListBuffer[Seq[GraphNode]]): Unit = {
     if (node.children.nonEmpty) {
       node.children.foreach(c => descend(c, parents :+ node, traces))
-
-    } else {
-      val reverse = (parents :+ node).reverse
-      traces += reverse
     }
   }
 
