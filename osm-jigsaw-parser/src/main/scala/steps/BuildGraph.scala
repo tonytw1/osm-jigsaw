f777763fa3ed8286b72a7eec21d85ccc3cabc137
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-12-29 17:30:07 +0000

    Clean ups.

--- osm-jigsaw-parser/src/main/scala/steps/BuildGraph.scala
+++ osm-jigsaw-parser/src/main/scala/steps/BuildGraph.scala
@@ -1,7 +1,7 @@
 package steps
 
-import java.util.concurrent.{Executors, ThreadPoolExecutor, TimeUnit}
 import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.{Executors, ThreadPoolExecutor, TimeUnit}
 
 import areas.AreaComparison
 import ch.hsr.geohash.GeoHash
@@ -15,21 +15,63 @@ import scala.collection.mutable.ListBuffer
 
 class BuildGraph extends OutputFiles with AreaReading with Segmenting with AreaComparison with Logging {
 
-  def buildGraph(extractName: String) = {
-    val areas = readAreasFromPbfFile(areasFilePath(extractName))
-
+  def buildGraph(extractName: String): Unit = {
     logger.info("Building graph")
 
-    val headArea = areas.head
-    val drop = areas
+    logger.info("Reading areas")
+    val areas = readAreasFromPbfFile(areasFilePath(extractName))
 
-    // Partiton into segments
-    val bounds = areas.map { a =>
+    // Partition into segments
+    val areaBoundingBoxes = areas.map { a =>
       boundingBoxFor(a.polygon)
     }
 
-    var bound = areas.head.boundingBox
-    bounds.foreach { b =>
+    val bound = boundsFor(areaBoundingBoxes)
+    logger.info("Bounding box to cover extract: " + bound)
+
+    val segmentSize = 4
+    val bb = new ch.hsr.geohash.BoundingBox(bound._3, bound._1, bound._2, bound._4)
+    val tt = TwoGeoHashBoundingBox.withCharacterPrecision(bb, segmentSize)
+    val i = new ch.hsr.geohash.util.BoundingBoxGeoHashIterator(tt)
+    val hashes = ListBuffer[GeoHash]()
+    while (i.hasNext) {
+      hashes += i.next()
+    }
+    logger.info("Need " + hashes.size + " segments to cover extract bounding box")
+
+    val planetPolygon = makePolygon((-180, 90), (180, -90))
+    val planet = Area(0, planetPolygon, boundingBoxFor(planetPolygon), ListBuffer.empty, areaOf(planetPolygon)) // TODO
+
+    logger.info("Mapping areas into segments")
+    val segments = segmentsFor(areas, hashes, segmentSize)
+
+    logger.info("Deduplicating segments")
+    val deduplicatedSegments = deduplicateSegments(segments)
+
+    logger.info("Processing segments")
+    val total = deduplicatedSegments.size
+
+    val availableHardwareThreads = Runtime.getRuntime.availableProcessors()
+    logger.info("Available processors: " + availableHardwareThreads)
+    val executor = Executors.newFixedThreadPool(availableHardwareThreads).asInstanceOf[ThreadPoolExecutor]
+
+    val doneCounter = new AtomicInteger(0)
+    deduplicatedSegments.map { segment =>
+      executor.submit(new SegmentTask(segment, extractName, planet, doneCounter, total))
+    }
+
+    logger.info("Requesting shutdown")
+    executor.shutdown()
+
+    logger.info("Awaiting shutdown")
+    executor.awaitTermination(5, TimeUnit.SECONDS)
+
+    logger.info("Done")
+  }
+
+  private def boundsFor(boundingBoxes: Seq[(Double, Double, Double, Double)]): (Double, Double, Double, Double) = {
+    var bound: (Double, Double, Double, Double) = boundingBoxes.head
+    boundingBoxes.foreach { b =>
       if (b._1 > bound._1) {
         bound = bound.copy(_1 = b._1)
       }
@@ -43,53 +85,7 @@ class BuildGraph extends OutputFiles with AreaReading with Segmenting with AreaC
         bound = bound.copy(_4 = b._4)
       }
     }
-    logger.info("Bounding box for cover extract: " + bound)
-
-    val bb = new ch.hsr.geohash.BoundingBox(bound._3, bound._1, bound._2, bound._4)
-
-    val segmentSize = 4
-    val tt = TwoGeoHashBoundingBox.withCharacterPrecision(bb, segmentSize)
-
-    val i = new ch.hsr.geohash.util.BoundingBoxGeoHashIterator(tt)
-    val hashes = ListBuffer[GeoHash]()
-    while (i.hasNext) {
-      val hash: GeoHash = i.next()
-      hashes += hash
-    }
-
-    logger.info("Need " + hashes.size + " segments to cover extract bounding box")
-
-    val planetPolygon = makePolygon((-180, 90), (180, -90))
-    val planet = Area(0, planetPolygon, boundingBoxFor(planetPolygon), ListBuffer.empty, areaOf(planetPolygon)) // TODO
-
-    val doneCounter = new AtomicInteger(0)
-
-    logger.info("Mapping areas into segments")
-    val segments = segmentsFor(drop, hashes, segmentSize)
-
-    logger.info("Deduplicating segments")
-    val deduplicatedSegments = deduplicateSegments(segments) // TODO backfill the deduplicated segments
-
-    logger.info("Processing segments")
-    val total = deduplicatedSegments.size
-
-    val availableHardwareThreads = Runtime.getRuntime.availableProcessors()
-    logger.info("Available processors: " + availableHardwareThreads)
-    val executor = Executors.newFixedThreadPool(availableHardwareThreads).asInstanceOf[ThreadPoolExecutor]
-
-    deduplicatedSegments.map { segment =>
-      val t = new SegmentTask(segment, extractName, planet, doneCounter, total)
-      val value = executor.submit(t)
-      value
-    }
-
-    logger.info("Requesting shutdown")
-    executor.shutdown()
-
-    logger.info("Awaiting shutdown")
-    executor.awaitTermination(5, TimeUnit.SECONDS)
-
-    logger.info("Done")
+    bound
   }
 
 }
