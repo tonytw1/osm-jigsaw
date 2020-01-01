8aadcec7aaf9fea576b54565b92b376cf7c0a66b
Tony McCrae <tony@eelpieconsulting.co.uk>
2020-01-01 11:00:28 +0000

    Split index and show WIP.

--- osm-jigsaw-viewer/app/controllers/Application.scala
+++ osm-jigsaw-viewer/app/controllers/Application.scala
@@ -17,59 +17,62 @@ class Application @Inject()(configuration: Configuration, ws: WSClient) extends 
   private val apiUrl = configuration.getString("api.url").get
   private val maxBoxApiKey = configuration.getString("mapbox.api.key").get
 
-  def index(qo: Option[String]) = Action.async { request =>
-    val q = qo.getOrElse("")
+  def index() = Action.async { request =>
+      Future.successful(Ok(views.html.index(maxBoxApiKey)))
+  }
 
+  def show(q: String, lat: Double, lon: Double) = Action.async { request =>
     //ws.url((apiUrl + "/show").addParam("q", q)).get.flatMap { r =>
-      implicit val er = Json.reads[Entity]
-      implicit val gnr = Json.reads[GraphNode]
-      val graphNodes: Seq[GraphNode] = Seq.empty // Json.parse(r.body).as[Seq[GraphNode]]
+    implicit val er = Json.reads[Entity]
+    implicit val gnr = Json.reads[GraphNode]
+    val graphNodes: Seq[GraphNode] = Seq.empty // Json.parse(r.body).as[Seq[GraphNode]]
 
-      val lastNode = graphNodes.lastOption
+    val lastNode = graphNodes.lastOption
 
-      val crumbs = lastNode.map { ln =>
-        areasToCrumbs(graphNodes)
+    val crumbs = lastNode.map { ln =>
+      areasToCrumbs(graphNodes)
+    }
+
+    val eventualAreaBoundingBox = lastNode.map { _ =>
+      ws.url((apiUrl + "/points").addParam("q", q)).get.map { psr =>
+        implicit val pr = Json.reads[Point]
+        val points = Json.parse(psr.body).as[Seq[Point]]
+        val b: (Double, Double, Double, Double) = boundingBoxFor(points)
+        Some(b)
       }
+    }.getOrElse {
+      Future.successful(None)
+    }
 
-      val eventualAreaBoundingBox = lastNode.map { _ =>
-        ws.url((apiUrl + "/points").addParam("q", q)).get.map { psr =>
-          implicit val pr = Json.reads[Point]
-          val points = Json.parse(psr.body).as[Seq[Point]]
-          val b: (Double, Double, Double, Double) = boundingBoxFor(points)
-          Some(b)
-        }
-      }.getOrElse {
-        Future.successful(None)
+    val osmUrls = lastNode.map { ln =>
+      ln.entities.map { e =>
+        val osmId = e.osmId
+        val osmTypes = Set("node", "way", "relation")
+        val osmType = osmId.takeRight(1).toLowerCase()
+        (osmId, "https://www.openstreetmap.org/" + osmTypes.find(t => t.startsWith(osmType)).getOrElse(osmType) + "/" + osmId.dropRight(1))
       }
+    }
 
-      val osmUrls = lastNode.map { ln =>
-        ln.entities.map { e =>
-          val osmId = e.osmId
-          val osmTypes = Set("node", "way", "relation")
-          val osmType = osmId.takeRight(1).toLowerCase()
-          (osmId, "https://www.openstreetmap.org/" + osmTypes.find(t => t.startsWith(osmType)).getOrElse(osmType) + "/" + osmId.dropRight(1))
-        }
-      }
-
-      val eventualTags: Future[Map[String, String]] = lastNode.flatMap { ln =>
-        ln.entities.headOption.map { e =>
-          val osmId = e.osmId
-          ws.url((apiUrl + "/tags").addParam("osm_id", osmId)).get.map { r =>
-            Json.parse(r.body).as[Map[String, JsValue]].map { i =>
-              (i._1, i._2.as[String])
-            }
+    val eventualTags: Future[Map[String, String]] = lastNode.flatMap { ln =>
+      ln.entities.headOption.map { e =>
+        val osmId = e.osmId
+        ws.url((apiUrl + "/tags").addParam("osm_id", osmId)).get.map { r =>
+          Json.parse(r.body).as[Map[String, JsValue]].map { i =>
+            (i._1, i._2.as[String])
           }
         }
-      }.getOrElse(Future.successful(Map.empty))
-
-      for {
-        areaBoundingBox <- eventualAreaBoundingBox
-        tags <- eventualTags
-      } yield {
-        Ok(views.html.index(lastNode, crumbs, osmUrls, maxBoxApiKey, areaBoundingBox, tags))
       }
+    }.getOrElse(Future.successful(Map.empty))
+
+    for {
+      areaBoundingBox <- eventualAreaBoundingBox
+      tags <- eventualTags
+    } yield {
+      Ok(views.html.show(lastNode, crumbs, osmUrls, maxBoxApiKey, areaBoundingBox, tags))
+    }
     //}
   }
+
 
   def click(lat: Double, lon: Double) = Action.async { request =>
     val reverseApiCallUrl = (apiUrl + "/reverse").addParam("lat", lat).addParam("lon", lon)
