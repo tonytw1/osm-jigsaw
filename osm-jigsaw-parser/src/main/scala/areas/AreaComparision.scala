bdfdaf8b6db5c0a0071d68a5109911807fe432cc
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-07-10 16:08:55 +0100

    Better definition of are same area resolves the cycles in the Ireland extract at the area deduplication phase.

--- osm-jigsaw-parser/src/main/scala/areas/AreaComparision.scala
+++ osm-jigsaw-parser/src/main/scala/areas/AreaComparision.scala
@@ -8,25 +8,17 @@ trait AreaComparison {
   val sr = SpatialReference.create(1)
 
   def areaContains(a: Area, b: Area): Boolean = {
-    val contains = a.convexHull.map { ch =>
+    a.convexHull.map { ch =>
       OperatorContains.local().execute(ch, b.polygon, sr, null) &&
         OperatorContains.local().execute(a.polygon, b.polygon, sr, null)
     }.getOrElse {
       OperatorContains.local().execute(a.polygon, b.polygon, sr, null)
     }
-    if (contains) {
-      !OperatorContains.local().execute(b.polygon, a.polygon, sr, null) // TODO this is a duplicate area check which should not have got this far
-    } else {
-      false
-    }
   }
 
   def areaSame(a: Area, b: Area): Boolean = {
-    if (a.boundingBox._3 < b.boundingBox._1 || a.boundingBox._1 > b.boundingBox._3 || a.boundingBox._2 < b.boundingBox._4 || a.boundingBox._4 > b.boundingBox._2) {
-      false
-    } else {
-      OperatorEquals.local().execute(a.polygon, b.polygon, sr, null)        // TODO Profile bounding boxes equal optimisation?
-    }
+      OperatorContains.local().execute(a.polygon, b.polygon, sr, null) &&
+        OperatorContains.local().execute(b.polygon, a.polygon, sr, null)
   }
 
   def areaOf(p: Polygon): Double = {
