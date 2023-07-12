8de6b134c2b70fad6f097892f8208a470d117a2b
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-07-12 22:11:06 +0100

    Note 180th meridian handling in OSM.
    We are largely ok; there are no wrap around areas we are mishandling. But we have suboptimal partitioning of areas which span the 180th.

--- osm-jigsaw-parser/src/main/scala/areas/AreaComparision.scala
+++ osm-jigsaw-parser/src/main/scala/areas/AreaComparision.scala
@@ -5,7 +5,9 @@ import model.Area
 
 trait AreaComparison {
 
-  val sr = SpatialReference.create(1)
+  val sr: SpatialReference = SpatialReference.create(1) // TODO wkid 1 is fast be we don't know what it is
+  // Handling of the 180th meridian in OSM; https://wiki.openstreetmap.org/wiki/180th_meridian
+  // We probably have areas which same this blocked on the top level.
 
   def areaContains(a: Area, b: Area): Boolean = {
     OperatorContains.local().execute(a.polygon, b.polygon, sr, null)
