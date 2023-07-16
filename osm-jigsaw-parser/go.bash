1340922ef54a3dec1057565e66a2d9bc72b7c89d
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-07-16 10:04:12 +0100

    Tweak output files; folder contains only the files needed by the API.

--- osm-jigsaw-parser/go.bash
+++ osm-jigsaw-parser/go.bash
@@ -1,15 +1,16 @@
+input="dorset-230715"
+
 # exit when any command fails
 set -e
 
-#input="great-britain-230704"
-input="ireland-and-northern-ireland-180717"
-input="malta-230704"
+mkdir -p $input
 
 jarfile="osm-jigsaw-parser-assembly-1.0.jar"
-
-#java -jar $jarfile -s boundaries $input
-#java -Xmx8G -jar $jarfile -s extract $input
-#java -Xmx8G -jar $jarfile -s areaways $input
-#java -Xmx8G -jar $jarfile -s areas $input
+java -jar $jarfile -s boundaries $input
+java -Xmx8G -jar $jarfile -s extract $input
+java -Xmx8G -jar $jarfile -s areaways $input
+java -Xmx8G -jar $jarfile -s areas $input
 time java -Xmx8G -jar $jarfile -s graph $input
-#java -jar $jarfile -s tags $input 
+java -jar $jarfile -s tags $input 
+java -jar $jarfile -s flip $input
+
