796302935e281b99167d8344c1fc8b01d8904546
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-01 12:58:47 +0000

    Confirming go script still works

--- osm-jigsaw-parser/go.bash
+++ osm-jigsaw-parser/go.bash
@@ -1,4 +1,4 @@
-input="malta-230704"
+input="new-zealand-241231"
 
 # exit when any command fails
 set -e
@@ -6,12 +6,14 @@ set -e
 mkdir -p $input
 
 jarfile="osm-jigsaw-parser-assembly-1.0.jar"
-#java -jar $jarfile -s boundaries $input
-#java -Xmx8G -jar $jarfile -s extract $input
-#java -Xmx8G -jar $jarfile -s areaways $input
-#java -Xmx8G -jar $jarfile -s areas $input
-#time java -Xmx8G -jar $jarfile -s graph $input
-#java -jar $jarfile -s tags $input 
-#java -jar $jarfile -s flip $input
+java -jar $jarfile -s boundaries $input
+java -Xmx16G -jar $jarfile -s extract $input
+java -Xmx16G -jar $jarfile -s areaways $input
+java -Xmx16G -jar $jarfile -s areas $input
 
-java -jar $jarfile -s tile $input
+time java -Xmx12G -jar $jarfile -s graph $input
+
+java -jar $jarfile -s tags $input
+java -jar $jarfile -s flip $input
+
+#time java -Xmx12G -jar $jarfile -s tile $input
